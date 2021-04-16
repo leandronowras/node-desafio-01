0512ab0571eb6ba428938102716cdb4870df8b48
Ludkao <l.leandro_abreu@hotmail.com>
2021-04-16 15:07:28 -0300

    desafio 01 completo

--- src/index.js
+++ src/index.js
@@ -10,20 +10,76 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404).json({message: 'Usuario nao encontrado'})
+  }
+
+  request.user = user
+
+  return next()
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request
+
+  if ((!user.pro & user.todos.length < 10) || user.pro) {
+    return next()
+  }
+
+  return response.status(403).json({message: 'usuario deve ter plano valido'})
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers
+  const {id} = request.params
+
+  // validacao do usuario pelo username
+  const user = users.find(user => user.username = username)
+  if (!user) {
+    return response.status(404)
+  }
+
+  // validando id
+  const validId = validate(id)
+  if (!validId) {
+    return response.status(400)
+  }
+
+  // verificando se id pertence ao todo
+  const todoId = user.todos.some(todo => todo.id === id)
+  if (!todoId) {
+    return response.status(404) 
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  // if (validId & todoId) {
+    request.user = user
+    request.todo = todo
+    return next()
+  // }
+
+  // return response.status(403).json({message: 'Todo invalido'})
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params
+
+  const user = users.find(user => user.id === id)
+
+  if (user) {
+    request.user = user
+    return next()
+  }
+
+  return response.status(404).json({message: "user not find"})
 }
+
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
